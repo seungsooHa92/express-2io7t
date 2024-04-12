c0fffe1ada0e700e7b71d877bcab4080a42f182b
seungsoo <43424322+seungsooHa92@users.noreply.github.com>
2024-04-12 21:52:29 +0900

    feat: dummy api 추가

--- app.js
+++ app.js
@@ -3,11 +3,25 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var vansRouter = require('./routes/vans')
 
 var app = express();
+var allowedDomains = ['http://localhost:5173', 'https://2io7t.netlify.app']; //TODO 바꿔야함
+var corsOptions = {
+  origin: function (origin, callback) {
+    // 요청이 허용된 도메인인지 확인
+    if (!origin || allowedDomains.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+};
+app.use(cors(corsOptions));// view engine setup
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -21,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/vans', vansRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
