e44d4e1b2d07a21fe748125abcbafc91a789e894
seungsoo <43424322+seungsooHa92@users.noreply.github.com>
2024-04-16 00:23:16 +0900

    feat: 의도된 에러 추가

--- app.js
+++ app.js
@@ -1,56 +1,60 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var cors = require("cors");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var vansRouter = require('./routes/vans')
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var vansRouter = require("./routes/vans");
+var errorRouter = require("./routes/error");
 
 var app = express();
-var allowedDomains = ['http://localhost:5173', 'https://front-2io7t.vercel.app'];
+var allowedDomains = [
+  "http://localhost:5173",
+  "https://front-2io7t.vercel.app",
+];
 var corsOptions = {
   origin: function (origin, callback) {
     // 요청이 허용된 도메인인지 확인
     if (!origin || allowedDomains.includes(origin)) {
       callback(null, true);
     } else {
-      callback(new Error('Not allowed by CORS'));
+      callback(new Error("Not allowed by CORS"));
     }
-  }
+  },
 };
-app.use(cors(corsOptions));// view engine setup
+app.use(cors(corsOptions)); // view engine setup
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/vans', vansRouter);
-
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/vans", vansRouter);
+app.use("/error", errorRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 
 module.exports = app;
